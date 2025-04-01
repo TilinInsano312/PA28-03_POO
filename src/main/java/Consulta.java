3bdf438e1fa7ad89d9eebf09a8eeaf7003b0af5d
Vicente Salazar <v.salazar11@ufromail.cl>
2025-04-01 00:54:19 -0300

    Creacion del objeto sistema, arreglo y coherencia de las clases
    Creacion de la clase sistema para el manejo de metodos
    pd: ya se me fue todo el orden

--- src/main/java/Consulta.java
+++ src/main/java/Consulta.java
@@ -3,36 +3,72 @@ import java.util.Scanner;
 public class Consulta {
     private String fecha;
     private String hora;
+    private String motivo;
     private Profesional profesional;
     private ArrayList<Paciente> pacientes;
     private ArrayList<Establecimiento> establecimientos;
     private ArrayList<Profesional> profesionals;
-    Scanner scanner = new Scanner(System.in);
 
-    public Consulta(String fecha, String hora, Profesional profesional, ArrayList<Paciente> pacientes, ArrayList<Establecimiento> establecimientos, ArrayList<Profesional> profesionals) {
+    public Consulta(String fecha, String hora, String nombrePaciente ,String apellidoPaciente, String dniPaciente, String motivo,  String nombreProfesional, String apellidoProfesional, String dniProfesional, String nombreEstablecimiento, String ubicacion, String tipoEstablecimiento) {
         this.fecha = fecha;
         this.hora = hora;
+        this.motivo = motivo;
+
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+
+    public String getMotivo() {
+        return motivo;
+    }
+
+    public void setMotivo(String motivo) {
+        this.motivo = motivo;
+    }
+
+    public Profesional getProfesional() {
+        return profesional;
+    }
+
+    public void setProfesional(Profesional profesional) {
         this.profesional = profesional;
+    }
+
+    public ArrayList<Paciente> getPacientes() {
+        return pacientes;
+    }
+
+    public void setPacientes(ArrayList<Paciente> pacientes) {
         this.pacientes = pacientes;
+    }
+
+    public ArrayList<Establecimiento> getEstablecimientos() {
+        return establecimientos;
+    }
+
+    public void setEstablecimientos(ArrayList<Establecimiento> establecimientos) {
         this.establecimientos = establecimientos;
+    }
+
+    public ArrayList<Profesional> getProfesionals() {
+        return profesionals;
+    }
+
+    public void setProfesionals(ArrayList<Profesional> profesionals) {
         this.profesionals = profesionals;
-    }
-
-    public void crearConsulta(String fecha, String hora, Profesional profesional, ArrayList<Paciente> pacientes, ArrayList<Establecimiento> establecimientos, ArrayList<Profesional> profesionals) {
-
-        Consulta consulta = new Consulta(fecha, hora, profesional, pacientes, establecimientos, profesionals);
-    }
-
-    @Override
-    public String toString() {
-        return "Consulta{" +
-                "fecha='" + fecha + '\'' +
-                ", hora='" + hora + '\'' +
-                ", profesional=" + profesional +
-                ", pacientes=" + pacientes +
-                ", establecimientos=" + establecimientos +
-                ", profesionals=" + profesionals +
-                ", scanner=" + scanner +
-                '}';
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-
+        Sistema sistema = new Sistema();
+        sistema.registrarPaciente();
+        sistema.registrarEstablecimiento();
+        sistema.registrarProfesional();
+        sistema.crearConsulta();
     }
 }
